af78f35770d783d9807dc93051a4c95d3defbc3e
Su Haris <sh.skyinfo@gmail.com>
2023-09-03 21:23:25 +0530

    Improve Network Information (#70)
    * Add Asian Location: Biznet ID
    
    * Improve the Network Information
    
    Sometimes from ip-api, either ISP or ASN is available, but it would not show since the condition is not satisfied. Fix to show if either value is present
    
    * Fix spacing issue

--- yabs.sh
+++ yabs.sh
@@ -12,7 +12,7 @@
 #             performance via fio. The script is designed to not require any dependencies
 #             - either compiled or installed - nor admin privileges to run.
 
-YABS_VERSION="v2023-04-23"
+YABS_VERSION="v2023-07-08"
 
 echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
 echo -e '#              Yet-Another-Bench-Script              #'
@@ -298,9 +298,15 @@ function ip_info() {
 	echo "$net_type Network Information:"
 	echo "---------------------------------"
 
-	if [[ -n "$isp" && -n "$as" ]]; then
+	if [[ -n "$isp" ]]; then
 		echo "ISP        : $isp"
+	else
+		echo "ISP        : Unknown"
+	fi
+	if [[ -n "$as" ]]; then
 		echo "ASN        : $as"
+	else
+		echo "ASN        : Unknown"
 	fi
 	if [[ -n "$org" ]]; then
 		echo "Host       : $org"
@@ -818,6 +824,7 @@ if [ -z "$SKIP_IPERF" ]; then
 		"nyc.speedtest.clouvider.net" "5200-5209" "Clouvider" "NYC, NY, US (10G)" "IPv4|IPv6" \
 		"dal.speedtest.clouvider.net" "5200-5209" "Clouvider" "Dallas, TX, US (10G)" "IPv4|IPv6" \
 		"la.speedtest.clouvider.net" "5200-5209" "Clouvider" "Los Angeles, CA, US (10G)" "IPv4|IPv6" \
+		"iperf.biznetnetworks.com" "5201-5203" "Biznet" "Jakarta, ID (10G)" "IPv4|IPv6"
 	)
 
 	# if the "REDUCE_NET" flag is activated, then do a shorter iperf test with only three locations
