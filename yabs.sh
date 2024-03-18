9b9c8b7ac4c3d239c40051b7c8ae8277385c3a05
Michael Hohl <me@michaelhohl.net>
2024-03-18 09:40:43 +0100

    Use Telia for Helsinki, FI instead of Hostkey
    HOSTKEY's iperf3 server seems to be limited to 1G. Another option in Helsinki would be Telia and they seem to be able to reach 10G without issues.

--- yabs.sh
+++ yabs.sh
@@ -808,7 +808,7 @@ if [ -z "$SKIP_IPERF" ]; then
 	IPERF_LOCS=( \
 		"lon.speedtest.clouvider.net" "5200-5209" "Clouvider" "London, UK (10G)" "IPv4|IPv6" \
 		"iperf-ams-nl.eranium.net" "5201-5210" "Eranium" "Amsterdam, NL (10G)" "IPv4|IPv6" \
-		"spd-fisrv.hostkey.com" "5200-5209" "HOSTKEY" "Helsinki, FI (10G)" "IPv4" \
+		"speedtest.extra.telia.fi" "5201-5208" "Telia" "Helsinki, FI (10G)" "IPv4" \
 		# AFR placeholder
 		"speedtest.uztelecom.uz" "5200-5209" "Uztelecom" "Tashkent, UZ (10G)" "IPv4|IPv6" \
 		"speedtest.sin1.sg.leaseweb.net" "5201-5210" "Leaseweb" "Singapore, SG (10G)" "IPv4|IPv6" \
